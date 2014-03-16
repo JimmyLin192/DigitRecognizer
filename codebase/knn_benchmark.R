386022075d14d9068d52f1b5facec9bc597815ea
jimmylin192 <linxin@gmail.com>
2014-03-16 00:08:55 -0500

    knn_benchmark.R

--- codebase/knn_benchmark.R
+++ codebase/knn_benchmark.R
@@ -8,6 +8,10 @@ test <- read.csv("../data/test.csv", header=TRUE)
 labels <- train[,1]
 train <- train[,-1]
 
+cat("KNN starts...")
 results <- (0:9)[knn(train, test, labels, k = 10, algorithm="cover_tree")]
+cat("KNN ends...")
 
+car("Write to external file starts...")
 write(results, file="knn_benchmark.csv", ncolumns=1) 
+cat("Write to external file ends...")
