2987f0a64e8e992f91889169e67397527a871208
linxin@gmail.com <linxin@gmail.com>
2014-03-29 19:24:22 -0500

    codebase result

--- codebase/PCA_KNN.R
+++ codebase/PCA_KNN.R
@@ -8,13 +8,11 @@ test <- read.csv("../data/PCAed_test.csv", header=FALSE)
 labels <- train[,1]
 train <- train[,-1]
 
-cat(nrow(train), nrow(test), "\n")
-cat(ncol(train), ncol(test), "\n")
-
 cat("KNN starts...")
 results <- (0:9)[knn(train, test, labels, k = 10, algorithm="cover_tree")]
 cat("KNN ends...")
 
 cat("Write to external file starts...")
-write(results, file="PCA_KNN.csv", ncolumns=1) 
+cbind(as.matrix(seq(nrow(results))), as.matrix(results))
+write.csv(results, file="PCA_KNN.csv", row.names=FALSE, col.names=FALSE) 
 cat("Write to external file ends...")
