f5684a91e0745c4d0e40f53a533096094498f34d
jimmylin192 <linxin@gmail.com>
2014-04-16 00:24:05 -0500

    before merged

--- codebase/PCA_libSVM.m
+++ codebase/PCA_libSVM.m
@@ -1,10 +1,10 @@
 function PCA_libSVM(trainFile, testFile, outLabels)
 
 [train_labels, train_instances] = libsvmread(trainFile);
-
 [test_labels, test_instances] = libsvmread(testFile);
 
+% TODO: add training option for multiclass classfication
 model = svmtrain(train_labels, train_instances);
-[predicted_labels, accuracy] = svmpredict(test_instances, test_labels);
+[predicted_labels] = svmpredict(test_labels, test_instances, model);
 libsvmwrite(outLabels, predicted_labels);
 end
